ad6945fd49faddf264e65bd0d1a56a9322fc68ff
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-06 13:56:56 +0300

    first commit

---
+++ stackada-stack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackada-stack
+//
+//  Created by Turcu Ciprian on 06/04/2017.
+//  Copyright © 2017 Turcu Ciprian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
